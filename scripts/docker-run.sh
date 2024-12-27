2fdbf70a5bd0f0fa4688cd7a1398bcca0f2ef1e2
Andrew Luzan <luzan.minsk@gmail.com>
2024-12-27 18:08:04 +0200

    allow to abort shutdown

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,6 +1,15 @@
+
+function ask_and_stop() {
+    # Shutdown in 30 seconds if not cancelled
+    read -t 30 -p "Shutdown in 30 seconds. Press Ctrl+C to cancel. "
+    if [ $? -eq 0 ]; then
+        ./scripts/stop.sh
+    fi
+}
+
+trap ask_and_stop EXIT SIGTERM
+
 docker run --gpus all --shm-size 128G --rm \
     --volume $DATA_DIR:/data \
     --volume $PWD:/lerobot \
     -it positronic/lerobot "$@"
-
-trap ./scripts/stop.sh EXIT
